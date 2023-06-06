4aa6a22605becd173c2e734419102a9a46ce3214
Mylene Febrissy <m.febrissy@gmail.com>
2023-06-06 22:21:49 +0200

    validations author/article models, #full_name in author model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :author
+
+  validates :title, :content, presence: true
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name.chr.capitalize}."
+  end
 end
