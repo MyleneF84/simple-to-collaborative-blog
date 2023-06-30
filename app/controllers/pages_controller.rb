43e136d7031d9eebeabc324ba3419b6de2004ce6
Mylene Febrissy <m.febrissy@gmail.com>
2023-06-30 13:13:14 +0200

    Clean navbar shared views, fix N+1 queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @articles = Article.first(8)
+    @articles = Article.eager_load(:authors, :tags).first(8)
   end
 end
