dd3e38a9915f2e242b24a7d2ea2e18f675c14610
Mylene Febrissy <m.febrissy@gmail.com>
2024-02-21 11:34:33 +0100

    CSS components created - article_card, btn

--- app/controllers/concerns/pages.rb
+++ app/controllers/concerns/pages.rb
@@ -1,6 +1,6 @@
 module Pages
   def home
-    @articles = Article.eager_load(:authors, :tags).first(8)
+    @articles = Article.eager_load(:authors, :tags).first(9)
     render 'pages/home'
   end
 end
